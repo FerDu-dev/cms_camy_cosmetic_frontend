53605007f18d0a657428a7c61ea02d7c13cc139a
Jose Pacheco <jpacheco@shokworks.io>
2023-10-06 00:11:18 -0400

    adding store in routing

---
+++ src/pages/StoreConfiguration/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const StoreConfiguration = () => {
+    return (<>
+        
+    </>)
+}
+
+export default StoreConfiguration;
---
+++ src/pages/StoreStock/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const StoreStock = () => {
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default StoreStock;
