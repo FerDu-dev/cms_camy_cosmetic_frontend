e255fc6760aba616271ca805aa22f7f06220ee05
Jose Pacheco <jpacheco@shokworks.io>
2023-10-05 23:46:56 -0400

    adding fix

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -1,12 +1,33 @@
 import React, { useState, useEffect} from "react";
 import AddProductForm from "../../components/AddProductsForm/add-product-form";
 import ProductsFilter from "../../components/ProductsFilter/products-filter";
+import { getProducts } from "../../api/products";
+import { Table } from "antd";
 import { Outlet } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 export const Product = () => {
-
+    const [products, setProducts] = useState([])
+    const [currentPage, setCurrentPage] = useState(1);
+    const [limit, setLimit] = useState(8);
+    const [loading, setLoading] = useState(false)
     const location = useLocation();
+    const columns = [
+        {
+          title: 'Foto',
+          dataIndex: 'picture_url',
+          key: 'picture_url',
+          render: picture_url => <img src={picture_url} alt="Brand" style={{width: '50px'}} />,
+        },
+        {
+          title: 'Nombre',
+          dataIndex: 'name',
+          key: 'name',
+        },
+      ];
 
+      const fetchProducts = async () => {
+        const response = await getProducts({})
+      }
     return (
         <>
           {location.pathname === '/producto'?
@@ -14,6 +35,7 @@ export const Product = () => {
             <>
                 <AddProductForm />
                 <ProductsFilter />
+                <Table dataSource={products} columns={columns} loading={loading} />
             </>
            )
 
